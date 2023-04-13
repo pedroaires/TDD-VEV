32c2b5bc54e51e29a22b372f2f4e675339a9353e
pedroaires <pedro.aires.cabral@gmail.com>
2023-04-12 22:02:20 -0300

    teste passando

--- calculadora_de_salario/src/main/java/org/example/CalculadoraDeSalario.java
+++ calculadora_de_salario/src/main/java/org/example/CalculadoraDeSalario.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class CalculadoraDeSalario {
+
+
+    public Double calculaSalarioLiquido(Funcionario funcionario){
+        return 0.0;
+    }
 }
--- calculadora_de_salario/src/test/java/org/example/CalculadoraDeSalarioTest.java
+++ calculadora_de_salario/src/test/java/org/example/CalculadoraDeSalarioTest.java
@@ -7,14 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraDeSalarioTest {
     private Funcionario desenvolvedor;
+    private Funcionario dba;
+    private Funcionario gerente;
+    private Funcionario testador;
+
+    private CalculadoraDeSalario sobreTeste;
     @BeforeEach
     void setUp() {
+        this.sobreTeste = new CalculadoraDeSalario();
+
         this.desenvolvedor = new Funcionario("Deyde", "deyde.costa", 3000.0, Cargo.DESENVOLVEDOR);
+        this.dba = new Funcionario("Deyde", "deyde.costa", 3000.0, Cargo.DBA);
+        this.gerente = new Funcionario("Deyde", "deyde.costa", 3000.0, Cargo.GERENTE);
+        this.testador = new Funcionario("Deyde", "deyde.costa", 3000.0, Cargo.TESTADOR);
     }
 
 
     @Test
     void deveRetornarZero(){
+        this.desenvolvedor.setSalarioBase(0.0);
+        this.dba.setSalarioBase(0.0);
+        this.gerente.setSalarioBase(0.0);
+        this.testador.setSalarioBase(0.0);
 
+        assertEquals(0.0, sobreTeste.calculaSalarioLiquido(desenvolvedor));
     }
 }
